395c2dcfc8d6ec7373271c78653afa8e14198007
Your Name <you@example.com>
2018-02-14 19:57:16 +0100

    Commit generated stuff

---
+++ desktop/src/de/slyh/soldiers/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.slyh.soldiers.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.slyh.soldiers.SoldiersMainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SoldiersMainGame(), config);
+	}
+}
---
+++ html/src/de/slyh/soldiers/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package de.slyh.soldiers.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import de.slyh.soldiers.SoldiersMainGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new SoldiersMainGame();
+        }
+}
