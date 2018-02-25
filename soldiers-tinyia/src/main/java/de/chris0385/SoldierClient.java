7f5ac60e76306b6349c176e1ae1c8cd7103c0822
Christophe Schmaltz <christophe.schmaltz@udg.de>
2018-02-25 18:09:58 +0100

    Updated client

--- soldiers-tinyia/src/main/java/de/chris0385/SoldierClient.java
+++ soldiers-tinyia/src/main/java/de/chris0385/SoldierClient.java
@@ -1,52 +1,23 @@
 package de.chris0385;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.Arrays;
-
-import org.eclipse.jetty.websocket.client.ClientUpgradeRequest;
-import org.eclipse.jetty.websocket.client.WebSocketClient;
-import org.eclipse.jetty.websocket.common.extensions.compress.PerMessageDeflateExtension;
 
 import de.chris0385.api.commands.MoveCommand;
 import de.chris0385.api.model.Id;
 
 public class SoldierClient {
 
-	
 	public static void main(String[] args) throws Exception {
-		WebSocketClient client = new WebSocketClient();
-//		client.getExtensionFactory().register("permessage-deflate", PerMessageDeflateExtension.class);
-		try {
-			
-			SoldierClientSocket socket = buildSocket(client);
+
+		try (SoldierClientSocket socket = SoldierClientSocket.createConnected("ws://localhost:5643/soldier")) {
 
 			socket.sendCommands(Arrays.asList(new MoveCommand(new Id("foo"), null)));
 			socket.sendCommands(Arrays.asList(new MoveCommand(new Id("bar"), null)));
-//			socket.sendObject(new MoveCommand(new Id("bar"), null));
+			// socket.sendObject(new MoveCommand(new Id("bar"), null));
 
 			Thread.sleep(10000l);
 
-		} finally {
-			try {
-				client.stop();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
 		}
 	}
 
-	private static SoldierClientSocket buildSocket(WebSocketClient client)
-			throws Exception, URISyntaxException, IOException, InterruptedException {
-		client.start();
-		String dest = "ws://localhost:8080/soldier";
-		SoldierClientSocket socket = new SoldierClientSocket();
-		URI echoUri = new URI(dest);
-		ClientUpgradeRequest request = new ClientUpgradeRequest();
-		request.addExtensions("permessage-deflate");
-		client.connect(socket, echoUri, request).get();
-//		socket.getLatch().await();
-		return socket;
-	}
 }
--- soldiers-tinyia/src/main/java/de/chris0385/SoldierClientSocket.java
+++ soldiers-tinyia/src/main/java/de/chris0385/SoldierClientSocket.java
@@ -1,30 +1,74 @@
 package de.chris0385;
 
 import java.io.IOException;
+import java.net.URI;
 import java.util.List;
-import java.util.concurrent.CountDownLatch;
+import java.util.function.Consumer;
 
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import org.eclipse.jetty.websocket.client.ClientUpgradeRequest;
+import org.eclipse.jetty.websocket.client.WebSocketClient;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import de.chris0385.api.commands.Command;
+import de.chris0385.api.messages.Message;
 
 @WebSocket
-public class SoldierClientSocket {
+public class SoldierClientSocket implements AutoCloseable {
 
-	private Session session;
+	private static final TypeReference TYPE = new TypeReference<List<Command>>() {
+			};
+	private Session session; 
 	private ObjectMapper mapper = new ObjectMapper();
+	private final WebSocketClient client;
+	private Consumer<Message> callback;
 
+	private SoldierClientSocket(WebSocketClient client) {
+		this.client = client;
+	}
+	
+	public static SoldierClientSocket createConnected(String dest) throws Exception {
+		return buildSocket(dest);
+	}
+	
+	public void setCallback(Consumer<Message> callback) {
+		this.callback = callback;
+		
+	}
+	
+	private static SoldierClientSocket buildSocket(String dest)
+			throws Exception {
+		WebSocketClient client = new WebSocketClient();
+//		client.getExtensionFactory().register("permessage-deflate", PerMessageDeflateExtension.class);
+		client.start();
+		SoldierClientSocket socket = new SoldierClientSocket(client);
+		URI serverURI = new URI(dest);
+		ClientUpgradeRequest request = new ClientUpgradeRequest();
+		request.addExtensions("permessage-deflate");
+		client.connect(socket, serverURI, request).get();
+		return socket;
+	}
 
 	@OnWebSocketMessage
-	public void onText(Session session, String message) throws IOException {
-		System.out.println("Message received from server:" + message);
+	public void onText(Session session, String messageS) throws IOException {
+		Message message = mapper.readValue(messageS, Message.class);
+		try {
+			if (callback != null) {
+				callback.accept(message);
+			} else {
+				System.out.println("Message received from server without callback:" + messageS);
+			}
+		} catch (Exception e) {
+			System.err.println("Error processing " + messageS);
+			System.err.println("Error is " + e);
+			e.printStackTrace();
+		}
 	}
 
 	@OnWebSocketConnect
@@ -35,29 +79,26 @@ public class SoldierClientSocket {
 
 	private void sendMessage(String str) {
 		try {
-			System.out.println("Send "+str);
 			session.getRemote().sendString(str);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			System.err.println("Connection error: " + e);
 			e.printStackTrace();
 		}
 	}
 	
 	public void sendCommands(List<Command> o) {
 		try {
-			sendMessage(mapper.writerFor(new TypeReference<List<Command>>() {
-			}).writeValueAsString(o));
+			sendMessage(mapper.writerFor(TYPE).writeValueAsString(o));
 		} catch (JsonProcessingException e) {
 			throw new RuntimeException(e);
 		}
 		
 	}
-	public void sendObject(Object o) {
-		try {
-			sendMessage(mapper.writeValueAsString(o));
-		} catch (JsonProcessingException e) {
-			throw new RuntimeException(e);
-		}
+
+	@Override
+	public void close() throws Exception {
+		// TODO Auto-generated method stub
+		client.stop();
 	}
 
 }
